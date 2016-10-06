f9dd517d8b47fa494a9ed964a4d3ca6bf2f92e04
spanicker <shubhie@gmail.com>
2016-10-06 15:44:59 -0700

    Create raf-observer.js

---
+++ raf-observer.js
@@ -0,0 +1,11 @@
+
+var observer = new PerformanceObserver(function(entryList) {
+  var entry = entryList.getEntries()[0];
+  var logBox = document.getElementById('eventlog');
+  if (logBox && entry.entryType == "longtask") {
+    var newItem = "long task! " + "start: " + entry.startTime + ", duration: " + (entry.duration / 1000) + "ms, name: " + entry.name + "<br>";
+    logBox.innerHTML = newItem + logBox.innerHTML;
+  };
+ );
+ observer.observe({entryTypes: ["longtask"]});
+
