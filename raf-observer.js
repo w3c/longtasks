f0f455acad2afc83cfdb71a22a703e80bb824630
Shubhie Panicker <panicker@chromium.org>
2016-10-11 21:21:59 -0700

    move observer to init

--- raf-observer.js
+++ raf-observer.js
@@ -1,13 +1,16 @@
+//function makeObserver() {
+  /*
+  var observer = new PerformanceObserver(function(entryList) {
+    var entry = entryList.getEntries()[0];
+    var logBox = document.getElementById('eventlog');
+    if (logBox && entry.entryType == "longtask") {
+      var newItem = "long task! " + "start: " + entry.startTime + ", duration: " + (entry.duration / 1000) + "ms, name: " + entry.name + "<br>";
+      console.log(newItem);
+      logBox.innerHTML = newItem + logBox.innerHTML;
+    };
+  });
 
-var observer = new PerformanceObserver(function(entryList) {
-  var entry = entryList.getEntries()[0];
-  var logBox = document.getElementById('eventlog');
-  if (logBox && entry.entryType == "longtask") {
-    var newItem = "long task! " + "start: " + entry.startTime + ", duration: " + (entry.duration / 1000) + "ms, name: " + entry.name + "<br>";
-    console.log(newItem);
-    logBox.innerHTML = newItem + logBox.innerHTML;
-  };
-});
-
-observer.observe({entryTypes: ["longtask"]});
+  observer.observe({entryTypes: ["longtask"]});
+  */
+//}
 
--- raf.js
+++ raf.js
@@ -22,4 +22,18 @@ function init() {
 
   var q = document.getElementById("stop");
   q.onclick = stopAnimating;
+
+  console.log('Make observer');
+  var observer = new PerformanceObserver(function(entryList) {
+    var entry = entryList.getEntries()[0];
+    var newItem = "long task! " + "start: " + entry.startTime + ", duration: " + (entry.duration / 1000) + "ms, name: " + entry.name;
+    console.log(newItem);
+    var logBox = document.getElementById('eventlog');
+    if (logBox && entry.entryType == "longtask") {
+      
+      logBox.innerHTML = newItem + "<br>" + logBox.innerHTML;
+    };
+  });
+  console.log('Observe longtask');
+  observer.observe({entryTypes: ["longtask"]});
 };
