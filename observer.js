7071345dbe10adec7c1959a38a166ad79dff220e
spanicker <shubhie@gmail.com>
2016-11-02 10:48:13 -0700

    Create observer.js

---
+++ observer.js
@@ -0,0 +1,14 @@
+
+    var observer = new PerformanceObserver(function(entryList) {
+      console.log('In PerformanceObserver');
+      var i = 0;
+      for (i = 0; i < entryList.getEntries().length; i++) {
+        var entry = entryList.getEntries()[i];
+        if (entry.entryType == "longtask") {
+          var item = "long task! " + "start: " + entry.startTime + ", duration: " + (entry.duration / 1000) + "ms, name: " + entry.name;
+          console.log(item);
+        }
+      }
+    });
+
+    observer.observe({entryTypes: ["longtask"]});
