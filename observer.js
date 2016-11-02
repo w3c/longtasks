f53cc64855f12f7ab55649297a1803252a4c3a26
spanicker <shubhie@gmail.com>
2016-11-02 10:55:50 -0700

    Update observer.js

--- observer.js
+++ observer.js
@@ -1,13 +1,13 @@
 
 function init() {
       window._observer = new PerformanceObserver(function(entryList) {
-      console.log('In PerformanceObserver');
+      //console.log('In PerformanceObserver');
       var i = 0;
       for (i = 0; i < entryList.getEntries().length; i++) {
         var entry = entryList.getEntries()[i];
         if (entry.entryType == "longtask") {
           var item = "long task! " + "start: " + entry.startTime + ", duration: " + (entry.duration / 1000) + "ms, name: " + entry.name;
-          console.log(item);
+          //console.log(item);
         }
       }
     });
