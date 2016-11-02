c0b5d8b622fbb46e66372e78efa00545185b992d
spanicker <shubhie@gmail.com>
2016-11-02 10:59:57 -0700

    Update randomslow.js

--- randomslow.js
+++ randomslow.js
@@ -6,8 +6,8 @@ function makeSlowTask(ms) {
 
 (function loop() {
     // Random number in range 100 - 400ms
-    var randTaskLen = Math.round(Math.random() * (400 - 10)) + 10;
-    var randDelay = Math.round(Math.random() * (1000 - 100)) + 100;
+    var randTaskLen = Math.round(Math.random() * (400 - 40)) + 10;
+    var randDelay = Math.round(Math.random() * (500 - 100)) + 100;
     setTimeout(function() {
             makeSlowTask(randTaskLen);
             loop();
