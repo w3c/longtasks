f7b5658e8b33dc0cff0d500459ea8f93c9aade4b
spanicker <shubhie@gmail.com>
2016-11-02 10:18:21 -0700

    Add random delays

--- init.js
+++ init.js
@@ -1,3 +1,20 @@
+
+function makeSlowTask(ms) {
+  var begin = window.performance.now();
+  while (window.performance.now() < begin + ms);
+}
+
+
+(function loop() {
+    // Random number in range 100 - 400ms
+    var randTaskLen = Math.round(Math.random() * (400 - 10)) + 10;
+    var randDelay = Math.round(Math.random() * (1000 - 300)) + 300;
+    setTimeout(function() {
+            makeSlowTask(randTaskLen);
+            loop();
+    }, randDelay);
+}());
+
 var globalID;
 
 function repeatOften() {
